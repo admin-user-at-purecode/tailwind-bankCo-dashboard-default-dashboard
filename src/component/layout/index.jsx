028d12e4a12fb872294299b70c80ce276debfca5
Shaan057 <141735478+Shaan057@users.noreply.github.com>
2024-02-22 16:58:18 +0530

    first commit

---
+++ src/component/layout/index.jsx
@@ -0,0 +1,49 @@
+import Sidebar from "./sidebar/index";
+import SidebarV2 from "./sidebar/SidebarV2";
+import Overlay from "./overlay";
+import HeaderOne from "./header/HeaderOne";
+import HeaderTwo from "./header/HeaderTwo";
+import { useState } from "react";
+import { createContext } from "react";
+import Home from "../../pages/DefaultDashboard";
+
+export const ThemeContext = createContext(null);
+
+function Layout({ bg, overlay }) {
+    const [sidebar, setSidebar] = useState(true);
+    const [theme, setTheme] = useState(
+        localStorage.getItem("theme") === "" || localStorage.getItem("theme")
+            ? localStorage.getItem("theme")
+            : ""
+    );
+
+    return (
+        <ThemeContext.Provider value={{ theme, setTheme }}>
+            <div
+                className={`layout-wrapper ${
+                    sidebar && "active"
+                }  w-full dark:bg-darkblack-600 `}
+                style={{
+                    borderColor: "#2a313c",
+                }}
+            >
+                <div className="relative flex w-full">
+                    <Sidebar handleActive={() => setSidebar(!sidebar)} />
+                    {overlay ? overlay : <Overlay />}
+                    <SidebarV2 />
+                    <div
+                        className={`body-wrapper flex-1 overflow-x-hidden ${
+                            bg ? bg : "dark:bg-darkblack-500"
+                        } `}
+                    >
+                        <HeaderOne handleSidebar={() => setSidebar(!sidebar)} />
+                        <HeaderTwo handleSidebar={() => setSidebar(!sidebar)} />
+                        <Home />
+                    </div>
+                </div>
+            </div>
+        </ThemeContext.Provider>
+    );
+}
+
+export default Layout;
