d827e9cf1e88360119f3e472bc6e7ce0c09845bb
Manish Gupta <manish.info2020@gmail.com>
2024-02-23 18:25:10 +0530

    created variant

--- src/pages/DefaultDashboard/index.jsx
+++ src/pages/DefaultDashboard/index.jsx
@@ -6,25 +6,24 @@ import TeamChat from "../../component/teamChat";
 import Efficiency from "../../component/revenueFlow/Efficiency";
 
 function Home() {
-    return (
-        <main className="w-full px-6 pb-6 pt-[100px] sm:pt-[156px] xl:px-12 xl:pb-12">
-            <div className="2xl:flex 2xl:space-x-[48px]">
-                <section className="mb-6 2xl:mb-0 2xl:flex-1">
-                    <TotalWidget />
-                    <div className="mb-[24px] w-full xl:flex xl:space-x-[24px]">
-                        <RevenueFlow />
-                        <Efficiency />
-                    </div>
-
-                    <ListTab />
-                </section>
-                <section className="flex w-full flex-col space-x-0 lg:flex-row lg:space-x-6 2xl:w-[400px] 2xl:flex-col 2xl:space-x-0">
-                    <Wallet />
-                    <TeamChat />
-                </section>
-            </div>
-        </main>
-    );
+  return (
+    <main className="w-full px-6 pb-6 pt-[100px] sm:pt-[156px] xl:px-12 xl:pb-12">
+      <div className="2xl:flex 2xl:space-x-[48px]">
+        <section className="mb-6 2xl:mb-0 2xl:flex-1">
+          <div className="mb-[24px] w-full xl:flex xl:space-x-[24px]">
+            <Efficiency />
+            <RevenueFlow />
+          </div>
+          <TotalWidget />
+          <ListTab />
+        </section>
+        <section className="flex w-full flex-col space-x-0 lg:flex-row lg:space-x-6 2xl:w-[400px] 2xl:flex-col 2xl:space-x-0">
+          <Wallet />
+          <TeamChat />
+        </section>
+      </div>
+    </main>
+  );
 }
 
 export default Home;
