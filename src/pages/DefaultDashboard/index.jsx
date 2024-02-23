211869a24194b152c3fa84757c99ec8da4027375
Manish Gupta <manish.info2020@gmail.com>
2024-02-23 19:58:42 +0530

    added variant

--- src/pages/DefaultDashboard/index.jsx
+++ src/pages/DefaultDashboard/index.jsx
@@ -10,13 +10,19 @@ function Home() {
         <main className="w-full px-6 pb-6 pt-[100px] sm:pt-[156px] xl:px-12 xl:pb-12">
             <div className="2xl:flex 2xl:space-x-[48px]">
                 <section className="mb-6 2xl:mb-0 2xl:flex-1">
-                    <TotalWidget />
+                <TotalWidget />
+
                     <div className="mb-[24px] w-full xl:flex xl:space-x-[24px]">
                         <RevenueFlow />
                         <Efficiency />
                     </div>
+                    <ListTab className="my-7" />
+                  <div className="my-5"><TotalWidget  /></div>  
 
-                    <ListTab />
+                    <div className="mb-[24px] w-full xl:flex xl:space-x-[24px] my-10">
+                    <Efficiency />
+                        <RevenueFlow />
+                    </div>
                 </section>
                 <section className="flex w-full flex-col space-x-0 lg:flex-row lg:space-x-6 2xl:w-[400px] 2xl:flex-col 2xl:space-x-0">
                     <Wallet />
