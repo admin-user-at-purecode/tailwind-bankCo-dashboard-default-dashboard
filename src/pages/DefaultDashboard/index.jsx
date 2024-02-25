b096b9f6489c07c0b91b766c889d31f81234db63
Manish Gupta <manish.info2020@gmail.com>
2024-02-25 10:57:03 +0530

    fixed spacing

--- src/pages/DefaultDashboard/index.jsx
+++ src/pages/DefaultDashboard/index.jsx
@@ -7,18 +7,19 @@ import TeamChat from "../../component/teamChat";
 function Home() {
   return (
     <main className="w-full px-6 pb-6 pt-[100px] sm:pt-[156px] xl:px-12 xl:pb-12">
-      <div className="2xl:flex 2xl:space-x-[48px]">
+      <div className="2xl:flex 2xl:space-x-[48px] mb-6">
         <section className="mb-6 2xl:mb-0 2xl:flex-1 w-full">
           <TotalWidget />
           <ListTab />
         </section>
         <section className="flex w-full flex-col space-x-0 lg:flex-row lg:space-x-6 2xl:w-[400px] 2xl:flex-col 2xl:space-x-0">
           <Wallet />
-          <TeamChat />
         </section>
       </div>
       <RevenueFlow />
+      <div className="my-8">
       <Wallet />
+      </div>
     </main>
   );
 }
