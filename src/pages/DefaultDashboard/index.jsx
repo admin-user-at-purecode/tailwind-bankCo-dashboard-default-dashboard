608943a9c10cb5d4983292ebe0eee53a7d2cec03
Manish Gupta <manish.info2020@gmail.com>
2024-02-25 12:40:43 +0530

    screenshots added

--- src/pages/DefaultDashboard/index.jsx
+++ src/pages/DefaultDashboard/index.jsx
@@ -1,8 +1,6 @@
 import TotalWidget from "../../component/widget/TotalWidget";
-import RevenueFlow from "../../component/revenueFlow";
-import ListTab from "../../component/listTab";
-import Wallet from "../../component/wallet";
 import TeamChat from "../../component/teamChat";
+import RevenueFlow from './../../component/revenueFlow/index';
 import Efficiency from "../../component/revenueFlow/Efficiency";
 
 function Home() {
@@ -11,9 +9,9 @@ function Home() {
             <div className="2xl:flex 2xl:space-x-[48px]">
                 <section className="mb-6 2xl:mb-0 2xl:flex-1">
                     <TotalWidget />
-                    <TotalWidget />
-                    <TotalWidget />
                     <div className="mb-[24px] w-full xl:flex xl:space-x-[24px]">
+                      <RevenueFlow/>
+                      <Efficiency/>
                     </div>
                 </section>
                 <section className="flex w-full flex-col space-x-0 lg:flex-row lg:space-x-6 2xl:w-[400px] 2xl:flex-col 2xl:space-x-0">
